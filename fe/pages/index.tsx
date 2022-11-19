e149a1d5846cce1653655eae6e20d7654ddae993
solozo <ozoemenamsolomon@yahoo.com>
2022-11-19 22:53:22 +0100

    configure next pwa - adjust user skills data -style login form - change user mail domain

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,24 +1,39 @@
-import Home1 from '../assets/home1.png'
-import Home2 from '../assets/home2.png'
+import Home1 from '../assets/home1.png';
+import Home2 from '../assets/home2.png';
 import Image from 'next/image';
 
 const Home = () => {
 	return (
-		<div style={{paddingLeft : "15px"}}>
-			
-			<div style={{margin: "0 auto", display : "table"}}>	
-			<div style={{paddingTop : "20px", paddingBottom : "20px"}}>
-				Im Fokus
-			</div >
-				
-				<Image src={Home1} alt="User Image" style={{
-				maxWidth : "100%"
-			}}  />
-				
-			</div>
-			<div style={{margin: "0 auto", display : "table", paddingTop : "60px"}}>
-				<Image src={Home2} alt="User Image" style={{width : "80%", height : "auto"}}  />
+		<div style={{ paddingLeft: '15px' }}>
+			<div style={{ margin: '0 auto', display: 'table' }}>
+				<div style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+					Im Fokus
+				</div>
 
+				<Image
+					src={Home1}
+					alt="User Image"
+					style={{
+						maxWidth: '95%',
+						objectFit: 'contain',
+						aspectRatio: '2.8',
+						width: '100%',
+						height: 'fit-content',
+					}}
+				/>
+			</div>
+			<div
+				style={{
+					margin: '0 auto',
+					display: 'table',
+					paddingTop: '60px',
+					textAlign: 'center',
+				}}>
+				<Image
+					src={Home2}
+					alt="User Image"
+					style={{ width: '80%', height: 'auto' }}
+				/>
 			</div>
 		</div>
 	);
